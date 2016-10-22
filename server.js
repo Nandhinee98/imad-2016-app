7b922ec14d4a72e1d8564478d924533b879e97ab
Nandhinee98 <novaash26@gmail.com>
2016-10-22 19:18:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/code-1',function(req,res){
+res.send('Code-1 requested');
+});
+
+app.get('/code-2',function(req,res){
+res.send('Code-2 requested');
+});
+
+app.get('/code-3',function(req,res){
+res.send('Code-3 requested');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
